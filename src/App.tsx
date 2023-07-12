e9b5ec88a31a50637ec92c891d3942bc486f2b3c
Sang Tran <t@sang.jp>
2023-07-12 14:21:05 +0900

    add fetch clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Fetch, useFetch} from 'clean-fetch'
+import {useState} from 'react'
+
+let d = 1
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [, rerender] = useState(0)
+	const {reload, data, error} = useFetch<number>(async () => {
+		await new Promise(resolve => setTimeout(resolve, 1000))
+		// throw new Error('hi')
+		d++
+		return d
+	})
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			hello
+			<div>
+				data: {JSON.stringify(data ?? 'undefined')}
+			</div>
+			<div>
+				error: {JSON.stringify(error?.message ?? 'undefined')}
+			</div>
+			<div>
+				<button onClick={reload}>Reload</button>
+			</div>
+			<div>
+				<button onClick={() => rerender(a => a + 1)}>Re-render</button>
+			</div>
+			<Fetch fetch={async () => {
+				await new Promise(resolve => setTimeout(resolve, 1000))
+				// throw new Error('hi')
+				d++
+				return d
+			}}>
+				{(data, reload) => <>
+					<div>
+						{JSON.stringify(data ?? 'undefined')}
+					</div>
+					<div>
+						<button onClick={reload}>Reload</button>
+					</div>
+				</>}
+			</Fetch>
+		</>
+	)
 }
 
 export default App
